5a78bb41a1f84078252f20b64afcedcbd51a7773
Tyler Nenninger <tylernenninger@Annas-Air.lan>
2022-09-13 11:46:06 -0500

    runtime

--- django_rest_api/django_rest_api/settings.py
+++ django_rest_api/django_rest_api/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import path
 import dj_database_url
 import os
 
